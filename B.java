8a923c03fb14a0c5f1b9d8b42d850a2801471db5
Divya Tirumala Korrapati <dk689@iris.nau.edu>
2024-06-11 16:12:37 -0700

    Merge 'math' branch into 'master'

--- B.java
+++ B.java
@@ -1,8 +1,11 @@
-public class B {
-
-
-}
-
+<<<<<<< HEAD
+// Changes from the current branch (master)
 public void masterMethod() {
-    
+    // Method implementation
 }
+=======
+// Changes from the other branch (math)
+public void bar() {
+    // Method implementation
+}
+>>>>>>> math
